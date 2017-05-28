7f941b8eb165c24a521c3590556ccd9f534d449d
Alice Dee <alice.dee@guardian.co.uk>
2017-05-28 23:21:30 +0100

    Scala coursera assignment 1

---
+++ recfun/src/test/scala/recfun/PascalSuite.scala
@@ -0,0 +1,32 @@
+package recfun
+
+import org.scalatest.FunSuite
+
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class PascalSuite extends FunSuite {
+  import Main.pascal
+  // test("pascal: col=0,row=2") {
+  //   assert(pascal(0,2) === 1)
+  // }
+
+  test("Where col=0, pascal = 1") {
+    assert(pascal(0,3) === 1)
+  }
+
+  test("Where col == row, pascal = 1") {
+    assert(pascal(2,2) === 1)
+  }
+
+  test("pascal: col=1,row=2") {
+    assert(pascal(1,2) === 2)
+  }
+
+  test("pascal: col=1,row=3") {
+    assert(pascal(1,3) === 3)
+  }
+
+}
